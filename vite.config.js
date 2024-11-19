b3075858eec025b7394455a8ceb88d7197686941
rajathshttgr <rajathshettigar.uy@gmail.com>
2024-11-19 23:32:06 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with the name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: 'contact-management'
-})
+  base: 'contact-management', 
+});
